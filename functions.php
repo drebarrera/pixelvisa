d6afd8b5b5fded647db1cc556dfb7d29ae97fc1d
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-10-08 17:01:16 +0700

    CICD Test 2

--- functions.php
+++ functions.php
@@ -105,7 +105,7 @@
         if ( empty($template_name) ) $template_name = basename($template, '.php');
         page_style($template_name);
         page_script($template_name);
-        echo "TEST";
+        echo "TEST 2";
     }
 
     function scripts() {
