26fae61a3bd3b68c1c2b3fb7570194150f1c2408
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-10-13 21:30:28 +0700

    Pulls photo from general assets for map

--- map.php
+++ map.php
@@ -2,11 +2,33 @@
 /*
 Template Name: Map Page
 */
+
+$portrait_url = "";
+$travel_icon_url = "";
+$biography = "";
+$travel_start = "";
+$args = array(
+    'post_type' => 'general_assets',
+    'posts_per_page' => 1,
+);
+$general_assets = new WP_Query( $args );
+if ( $general_assets->have_posts() ) {
+    while ( $general_assets->have_posts() ) {
+        $general_assets->the_post();
+        $portrait = get_field("portrait-image");
+        if ( !empty($portrait) ) $portrait_url = $portrait["url"];
+        $travel_icon = get_field("icon-image");
+        if ( !empty($travel_icon) ) $travel_icon_url = $travel_icon["url"];
+        $biography = get_field("biography");
+        $travel_start = get_field("travel-start");
+    }
+}
+
 get_header();
 ?>
 <section id="map-section">
     <div class="content">
-        <div id="map"></div>
+        <div id="map" style="--bg: url(' <?php echo $travel_icon_url ?> ')"></div>
         <div id="panel"></div>
     </div>
 </section>
