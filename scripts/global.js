6b476457ebfaf5e40ecc4200dd99d4e1b81b709f
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-10-14 17:28:50 +0700

    CSS map for mobile

--- scripts/global.js
+++ scripts/global.js
@@ -1,12 +1,12 @@
 function headerDown(header) {
-    if (window.innerWidth < 768) {
-        header.classList.add("headerDown");
+    if (window.innerWidth < 768 && !header.classList.contains("headerDown")) {
+            header.classList.add("headerDown");
     }
 }
 
 document.addEventListener('click', (event) => {
     const header = document.getElementsByTagName("header")[0];
-    if (!event.target.closest('.headerDown')) {
+    if (header.classList.contains("headerDown")) {
         header.classList.remove('headerDown');
     }
 });
