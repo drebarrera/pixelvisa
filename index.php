b1412b60a61145d780490087c538adc965d1fdf7
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-11-18 18:21:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             $general_assets->the_post();
             $portrait = get_field("portrait-image");
             if ( !empty($portrait) ) $portrait_url = $portrait["url"];
-            echo image_array($portrait, "medium");
+            echo image_array($portrait, "large");
             $travel_icon = get_field("icon-image");
             if ( !empty($travel_icon) ) $travel_icon_url = $travel_icon["url"];
             $biography = get_field("biography");
