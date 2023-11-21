a0bb6e75762a1db704c2ded4be41e7f04a96c50e
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-11-21 19:21:45 +0700

    Update map-leaflet.js

--- scripts/leaflet/map-leaflet.js
+++ scripts/leaflet/map-leaflet.js
@@ -265,6 +265,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 entry_type.innerHTML = food_svg + " Local Bite";
                 entry_button.textContent = "Taste " + entry["food-restaurant"];
                 entry_button.href = entry["food-url"];
+                console.log(entry);
             } else {
                 entry_name.textContent = "Arrived in " + panel_data[datum]["city"];
                 entry_type.innerHTML = visit_svg + " Visit";
