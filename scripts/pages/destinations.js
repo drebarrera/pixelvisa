845f425bdee869bd113e6f97cf0573ed031d3334
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-11-25 16:05:34 +0700

    Destinations Page Set Up

---
+++ scripts/pages/destinations.js
@@ -0,0 +1,41 @@
+function select_region(region) {
+    const region_cards = document.getElementsByClassName("region-card");
+    for (let i = 0; i < region_cards.length; i++) {
+        region_cards[i].style.display = "none";
+    };
+    
+    const choose_identifier = document.getElementById("choose-identifier");
+    choose_identifier.textContent = "Choose a Country";
+
+    const back_button = document.getElementById("back-button");
+    back_button.style.display = "flex";
+
+    const country_cards = document.getElementsByClassName("country-card");
+    var j = 0;
+    for (let i = 0; i < country_cards.length; i++) {
+        if (country_cards[i].dataset.region == region) {
+            setTimeout(function() {
+                country_cards[i].style.display = "flex";
+            }, j * 250);
+            j++;
+        }
+    };
+}
+
+function back_to_regions() {
+    const country_cards = document.getElementsByClassName("country-card");
+    for (let i = 0; i < country_cards.length; i++) {
+        country_cards[i].style.display = "none";
+    };
+
+    const choose_identifier = document.getElementById("choose-identifier");
+    choose_identifier.textContent = "Choose a Region";
+
+    const back_button = document.getElementById("back-button");
+    back_button.style.display = "none";
+
+    const region_cards = document.getElementsByClassName("region-card");
+    for (let i = 0; i < region_cards.length; i++) {
+        region_cards[i].style.display = "flex";
+    };
+}
