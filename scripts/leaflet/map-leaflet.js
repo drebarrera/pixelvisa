4240fc2c429b2aeb05a9db8e5e9e5ce05a1e16d6
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-10 15:55:35 +0700

    Update map-leaflet.js

--- scripts/leaflet/map-leaflet.js
+++ scripts/leaflet/map-leaflet.js
@@ -7,7 +7,6 @@ function scrollIntoParentView(element, parent) {
     } else if (elementRect.top > parentRect.top) {
         parent.scrollTop += (elementRect.top - parentRect.top);
     }
-    //parent.scrollTop = elementRect.top;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -142,7 +141,7 @@ document.addEventListener("DOMContentLoaded", function() {
             markerElement.dataset.markerid = marker_data[i][3].toString();
             markers[marker_data[i][3].toString()] = marker;
             // Bind popup to marker
-            //marker.bindPopup("<b>" + visit_svg + marker_datum[1] + " " + marker_datum[2] + "</b>", { autoPan: false }).openPopup();
+            marker.bindPopup("<b>" + visit_svg + marker_datum[1] + " " + marker_datum[2] + "</b>", { autoPan: false }).openPopup();
             
             // Bind marker click event
             marker.on('click', function(e) {
