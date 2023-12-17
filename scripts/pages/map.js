f24c0782cc5837d179f0e4c26af1e6e95512db0e
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-17 12:37:37 +0700

    Update map.js

--- scripts/pages/map.js
+++ scripts/pages/map.js
@@ -1,14 +1,25 @@
 var toggled = [];
+var filtered = [];
 
 function filter(button) {
     if (button.classList.contains("filter-button-toggled")) {
         button.classList.remove("filter-button-toggled");
         var index = toggled.indexOf(button.dataset.entrytype);
         if (index !== -1) toggled.splice(index, 1);
-        console.log(toggled);
     } else {
         button.classList.add("filter-button-toggled");
         toggled.push(button.dataset.entrytype);
-        console.log(toggled);
     }
+
+    var entries = document.querySelectorAll('.entry-item');
+    entries.forEach(function(entry) {
+        if (toggled.length > 0) {
+            entry.style.display = "none";
+            toggled.forEach(function(entrytype) {
+                if (entrytype == entry.dataset.entrytype) toggle = entry.style.display = "flex";
+            });
+        } else {
+            entry.style.display = "flex";
+        }
+    });
 }
