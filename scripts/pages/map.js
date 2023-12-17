523b4e6f8e486a766bd2bb3056660ed4210c49ab
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-17 12:24:11 +0700

    Update map.js

--- scripts/pages/map.js
+++ scripts/pages/map.js
@@ -1,7 +1,13 @@
+var toggled = [];
+
 function filter(button) {
     if (button.classList.contains("filter-button-toggled")) {
         button.classList.remove("filter-button-toggled");
+        toggled.remove(button.dataset.entrytype);
+        console.log(toggled);
     } else {
         button.classList.add("filter-button-toggled");
+        toggled.add(button.dataset.entrytype);
+        console.log(toggled);
     }
 }
