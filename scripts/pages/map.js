bb76f45e10c0006b42e239afc9b350605696ffcc
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-17 12:26:59 +0700

    Update map.js

--- scripts/pages/map.js
+++ scripts/pages/map.js
@@ -3,7 +3,7 @@ var toggled = [];
 function filter(button) {
     if (button.classList.contains("filter-button-toggled")) {
         button.classList.remove("filter-button-toggled");
-        var index = toggled.indexOf(itemToRemove);
+        var index = toggled.indexOf(button.dataset.entrytype);
         if (index !== -1) toggled.splice(index, 1);
         console.log(toggled);
     } else {
