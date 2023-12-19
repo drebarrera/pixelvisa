db41e2b7a5f047394c5f457eafc5d536f9b9ca23
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-19 16:44:56 +0700

    Update map.js

--- scripts/pages/map.js
+++ scripts/pages/map.js
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 toggle(document.querySelector(searchtype));
                 searchResults[searchtype] = [];
                 searchItem.addEventListener("click", function(e) {
+                    alert("hi");
                     input.value = entry[1][1];
                     search(searchtype, input);
                     document.querySelector(searchtype + " input").dataset.validinput = "true";
