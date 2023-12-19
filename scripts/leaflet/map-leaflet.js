e0edab3a17b68a70fb227605427033c2bed1f5d4
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-12-19 15:11:14 +0700

    Update map-leaflet.js

--- scripts/leaflet/map-leaflet.js
+++ scripts/leaflet/map-leaflet.js
@@ -207,11 +207,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const location_country_a = document.createElement("a"); // Flag & Country subheader
         location_country_a.textContent = panel_data[datum].flag + " "  + panel_data[datum].country;
         if (!countries.hasOwnProperty(panel_data[datum].country.toLowerCase())) countries[panel_data[datum].country.toLowerCase()] = [panel_data[datum].country, [location_entry.dataset.markerid]];
-        else {
-            console.log(countries[panel_data[datum].country.toLowerCase()], countries[panel_data[datum].country.toLowerCase()].length, panel_data[datum].country.toLowerCase());
-            countries[panel_data[datum].country.toLowerCase()][1].append(location_entry.dataset.markerid);
-        
-        }
+        else countries[panel_data[datum].country.toLowerCase()][1].push(location_entry.dataset.markerid);
         location_country_a.href = panel_data[datum]["country-url"];
         location_country_a.className = "location-country-p";
         location_entry.appendChild(location_country_a);
